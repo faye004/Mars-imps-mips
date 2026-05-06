baa33740d32f9ef0307d2435ca749257cdb76c80
Faye Kenoua Diop <kenouadiop@gmail.com>
2026-05-06 11:31:56 +0200

    fix(): File fixed, error with the RIPRISTINA ARRAY LOOPS, I've written different solutions to avoid the problem (using sub, mul and/or shift

--- l3exe1.asm
+++ l3exe1.asm
@@ -21,21 +21,45 @@ LOOP1:	lw $t0, 0($s0)
 	syscall
 	
 	bne $t8, $t9, LOOP1
+	li $t8, 0
 	
+#ripristono puntatore complicato e spagliato, guarda gli altri
+#RIPRISTINA_ARRAY:
+#		addi $s0, $s0, -4
+#		addi $t8, $t8, 1
+#		bne $t8, $t9, RIPRISTINA_ARRAY
+    
+    #suoluzione mia veloce
+    #li $t0, -4
+    #mul $t0, $t0, $t9
+    #add $s0, $s0, $t0
+    
+    #soluzione più veloce
+    #li $t0, 4
+#mul $t0, $t0, $t9   # t0 = size * 4
+#sub $s0, $s0, $t0   # s0 torna indietro
+    
+    #oppure usando lo shift
+    sll $t0, $t9, 2   # t0 = t9 * 4
+sub $s0, $s0, $t0  #ripristini il puntatore
 
-    # Sum all elements
-LOOP2:	lw $t0, 0($s0)
-	subi $s0, $s0, 4
-	subi $t8, $t8, 1
-	
-	move $a0, $t0
-	li $v0, 1
-	syscall
-	
-	bne $t8, 1, LOOP1
+ # Sum all elements
+    li $t1, 0
+    li $t8, 0
+    
+SUM_ARRAY:	lw $t0, 0($s0)
+		add $t1, $t1, $t0
+		addi $s0, $s0, 4
+		addi $t8, $t8, 1
+		bne $t8, $t9, SUM_ARRAY
 
     # Print the result
-
+    li $v0, 4
+    la $a0, result_msg
+    syscall
+    move $a0, $t1
+li $v0, 1
+syscall
     # Exit
 li $v0, 10
 syscall
